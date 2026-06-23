c8882d5b982354eea2d34ea64da8b76f0b934a6a
SHAKTI SINGH SHEKHAWAT <162464537+shaktisinghshekhawat100@users.noreply.github.com>
2026-06-23 14:13:30 +0000

    g

---
+++ basics/patterns.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std;
+void print1(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void print2(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void print3(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+void print4(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+void print5(int n)
+{
+    for(int i=n;i>=0;i--)
+    {
+        for(int j=i;j>0;j--)
+        {
+        
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void print6(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            cout << " ";
+        }
+        for(int j=1;j<=i;j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cout << "Enter the number of test cases: ";
+    cin >> t;
+    for (int i=0;i<t;i++)
+    {
+        int n;
+        cout << "Enter the number of rows: ";
+        cin >> n;
+        print5(n);
+    }
+   
+}
+
