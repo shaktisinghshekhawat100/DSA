74232b8c6e04ef1bb93eac07f636ba90a0ad3892
SHAKTI SINGH SHEKHAWAT <shaktidesktop@gmail.com>
2026-06-25 10:31:22 +0530

    11

--- basics/patterns.cpp
+++ basics/patterns.cpp
@@ -70,6 +70,80 @@ void print6(int n)
 }
 void print7(int n)
 {
+    //pyramid pattern
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout << " ";
+        }
+        for(int j=0;j<2*i+1;j++)
+        {
+            cout << "*";
+        }
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print8(int n){
+    // inverted pyramid pattern
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout << " ";
+        }
+        for(int j=0;j<2*(n-i)-1;j++)
+        {
+            cout << "*";
+        }
+        for(int j=0;j<i;j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print9(int n)
+{
+   for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout << " ";
+        }
+        for(int j=0;j<2*i+1;j++)
+        {
+            cout << "*";
+        }
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+         for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout << " ";
+        }
+        for(int j=0;j<2*(n-i)-1;j++)
+        {
+            cout << "*";
+        }
+        for(int j=0;j<i;j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print10(int n)
+{
     for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=i;j++)
@@ -87,6 +161,25 @@ void print7(int n)
         cout << endl;
     }
 }
+void print11(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            if((i+j)%2==0)
+            {
+                cout << "1 ";
+            }
+            else
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+
+}     
 int main()
 {
     int t;
@@ -97,7 +190,7 @@ int main()
         int n;
         cout << "Enter the number of rows: ";
         cin >> n;
-        print7(n);
+        print11(n);
     }
     return 0;
    
