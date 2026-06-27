bec1faa3eff441ebd55ebc1ba14346c122f3112b
SHAKTI SINGH SHEKHAWAT <shaktidesktop@gmail.com>
2026-06-27 18:26:55 +0530

    3

---
+++ basics/basic maths/Reverse Digits of A Number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n) {
+    int rev = 0;
+    while (n > 0) {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+    return rev;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int reversed = reverseNumber(n);
+    cout << "Reversed number: " << reversed << endl;
+    return 0;
+}
