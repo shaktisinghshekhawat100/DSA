5b460f0c42f3edcff61ef9b2391bb30a9c67a18b
SHAKTI SINGH SHEKHAWAT <shaktidesktop@gmail.com>
2026-06-27 18:41:14 +0530

    n

--- basics/basic maths/Reverse Digits of A Number.cpp
+++ basics/basic maths/Reverse Digits of A Number.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int reverseNumber(int n) {
     int rev = 0;
     while (n > 0) {
-        rev = rev * 10 + n % 10;
+        int digit = n % 10;
+        rev = rev * 10 + digit;
         n /= 10;
     }
     return rev;
